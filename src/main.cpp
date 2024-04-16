2430152871db453f8cf7df6e912ae10e83d04ead
Alberto Nidasio <alberto.nidasio@skywarder.eu>
2024-04-16 10:36:55 +0200

    Added MxGui

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <kernel/kernel.h>
+#include <mxgui/display.h>
+
+using namespace mxgui;
 
 int main()
 {
+    {
+        DrawingContext dc(DisplayManager::instance().getDisplay());
+        dc.write(Point(0, 0), "Hello world");
+    }
+
     while (true)
     {
         printf("Hi mom\n");
